349d45b2dc67098c0c509a99943b4f755cbd677b
Webyoza laptop <webyozalaptop@Webyozas-MacBook-Pro.local>
2023-12-26 11:28:26 +0100

    Fixed path for settings

--- zcy_donation/settings/base.py
+++ zcy_donation/settings/base.py
@@ -23,12 +23,6 @@ load_dotenv(env_path)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
