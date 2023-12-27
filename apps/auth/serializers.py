7cbffabb218e05b22cb2db360cf3cd7d811c77b2
glodanm <glodan196915@gmail.com>
2023-12-27 13:57:38 +0200

    add additional fields for user registration

--- apps/auth/serializers.py
+++ apps/auth/serializers.py
@@ -3,19 +3,31 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.settings import api_settings
 from django.contrib.auth.models import update_last_login
 
-from apps.user.models import User
+from apps.user.models import User, VolunteerInfo
 from apps.user.serializers import UserSerializer
 
 
+class CustomProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = VolunteerInfo
+        fields = ['public_name', 'first_name', 'last_name']
+
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    user = CustomProfileSerializer(write_only=True)
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'user']
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        user_data = validated_data.pop('user')
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data, password=password)
+        volunteer_info = VolunteerInfo.objects.create(user=user, **user_data)
+        return user
     
 
 class UserLoginSerializer(TokenObtainPairSerializer):
