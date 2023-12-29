3591ba2c191988d3080436e4bb2ac6abf4c012db
Sergey-sg <glushaksergey90@gmail.com>
2023-12-29 11:57:32 +0200

    left only my code

--- apps/jars/urls.py
+++ apps/jars/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-import apps.jars.views
+
+from . import views
+
 
 urlpatterns = [
-    # path('', views.all_jars, name='jars_list'),
     path('', views.JarsListView.as_view(), name='jars_list'),
-    path('tags/', views.jars_tag_filter),
-    path('create/', views.JarCreateView.as_view(), name='jar_create')
-    path('', views.AllJarsView.as_view(), name='all-jars-view'),
-    path('tags/', views.JarByTagView.as_view(), name='jars-tag-filter-view')
+    path('create/', views.JarCreateView.as_view(), name='jar_create'),
 ]
--- apps/jars/views.py
+++ apps/jars/views.py
@@ -1,46 +1,9 @@
-from rest_framework import generics, views, status
-
-from django.core.serializers import serialize
-from django.urls import reverse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from django.http import (
-    HttpResponseBadRequest,
-    HttpResponseRedirect,
-    JsonResponse,
-    HttpResponseServerError
-)
-from rest_framework.response import Response
 
-from apps.jars import serializers
-from apps.jars.models import Jar
-from apps.jars.serializers import JarSerializer
-
-from .models import Jar, JarTag
+from .models import Jar
 from .serializers import JarsSerializer, JarCreateSerializer
-
-
-class AllJarsView(generics.ListAPIView):
-    queryset = Jar.objects.all()
-    serializer_class = serializers.JarSerializer
-
-
-class JarByTagView(views.APIView):
-    """
-    View to retrieve a list of jars filtered by tag.
-    """
-
-    def get(self, request):
-        tag = request.GET.get('tag__name')
-        if tag:
-            jars_with_tag = Jar.objects.filter(tags__name=tag)
-            serializer = JarSerializer(jars_with_tag, many=True)
-
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response("redirect: 'jars/'")
-
 
 
 class JarsListView(generics.ListAPIView):
