fa0bdddc8e7ccdf520b80d0efb64d6277ad4418d
Sergey-sg <glushaksergey90@gmail.com>
2023-12-29 10:02:18 +0200

    fixed JarCreateSerializer

--- apps/jars/serializers.py
+++ apps/jars/serializers.py
@@ -26,6 +26,19 @@ class JarTagSerializer(serializers.ModelSerializer):
 
 
 class JarCurrentSumSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the current sum of a Jar model.
+
+    Fields:
+        - `sum` (int): The current sum in the jar.
+
+    Example:
+    ```json
+    {
+        "sum": 500
+    }
+    ```
+    """
     class Meta:
         model = JarCurrentSum
         fields = ['sum']
@@ -33,15 +46,15 @@ class JarCurrentSumSerializer(serializers.ModelSerializer):
 
 class JarsSerializer(serializers.ModelSerializer):
     """
-    Serializer for Jar model.
+    Serializer for the Jar model.
 
     Fields:
-        - `monobank_id`: Monobank ID of the jar.
-        - `title`: Title of the jar.
-        - `tags`: List of tags associated with the jar (serialized using JarTagSerializer).
-        - `goal`: Goal sum of the jar.
-        - `current`: Current sum in the jar.
-        - `date_added`: Date when the jar was added.
+        - `monobank_id` (str): The ID of the jar in Monobank.
+        - `title` (str): The title of the jar.
+        - `tags` (list): List of tags associated with the jar (serialized using JarTagSerializer).
+        - `goal` (int): Goal sum of the jar.
+        - `current_sums` (list): List of current sums in the jar (serialized using JarCurrentSumSerializer).
+        - `date_added` (datetime): Date and time when the jar was added.
 
     Example:
     ```json
@@ -53,7 +66,7 @@ class JarsSerializer(serializers.ModelSerializer):
             {"name": "category2"}
         ],
         "goal": 1000,
-        "current": 500,
+        "current_sums": [{"sum": 500}, {"sum": 600}],
         "date_added": "2023-01-01T12:00:00Z"
     }
     ```
@@ -80,13 +93,15 @@ class JarCreateSerializer(serializers.ModelSerializer):
     {
         "monobank_id": "1234567890",
         "title": "Savings Jar",
-        "tags": [1, 2]
+        "tags": ["tag1", "tag2"]
     }
     ```
     """
     class Meta:
         model = Jar
         fields = ['monobank_id', 'title', 'tags']
+
+    tags = serializers.ListField(write_only=True)
 
     def create(self, validated_data) -> HttpResponseForbidden | Jar:
         tags_data = validated_data.pop('tags')
@@ -99,8 +114,12 @@ class JarCreateSerializer(serializers.ModelSerializer):
         jar = Jar.objects.create(**validated_data)
 
         for tag_data in tags_data:
-            tag = JarTag.objects.get(name=tag_data)
-            jar.tags.add(tag)
+            try:
+                tag = JarTag.objects.get(name=tag_data)
+            except JarTag.DoesNotExist:
+                pass
+            else:
+                jar.tags.add(tag)
 
         jar.save()
 
