e1a488f05a280189b036af76662353d32f3b53fe
Sergey-sg <glushaksergey90@gmail.com>
2024-01-02 21:05:51 +0200

    Fixed PR

--- apps/jars/filters.py
+++ apps/jars/filters.py
@@ -10,7 +10,24 @@ FILTER_CHOICES = (
 
 
 class JarFilter(filters.FilterSet):
-    """filter for Jar"""
+    """
+    Filter for Jar model.
+
+    Supports filtering by fill percentage.
+
+    Example:
+    ```
+    /api/jars/?fill_percentage=-fill_percentage
+    ```
+
+    Query Parameters:
+        - `fill_percentage`: Filter jars by fill percentage.
+
+    Choices:
+        - "fill_percentage": Ascending order
+        - "-fill_percentage": Descending order
+
+    """
     fill_percentage = filters.ChoiceFilter(
         choices=FILTER_CHOICES,
         method='filter_fill_percentage'
@@ -21,12 +38,17 @@ class JarFilter(filters.FilterSet):
         fields = ['fill_percentage']
 
     def filter_fill_percentage(self, queryset, name, value) -> QuerySet:
-        # filtered_queryset = queryset.annotate(
-        #         fill_percentage=ExpressionWrapper(
-        #             F('jarcurrentsum__sum') * 100.0 / F('goal'),
-        #             output_field=fields.FloatField()
-        #         )
-        #     ).order_by(value)
+        """
+        Filter jars based on fill percentage.
+
+        Args:
+            queryset (QuerySet): The queryset to be filtered.
+            name (str): The name of the filter field.
+            value (str): The selected filter value.
+
+        Returns:
+            QuerySet: The filtered queryset.
+        """
         subquery = JarCurrentSum.objects.filter(jar=OuterRef('pk')).order_by('-date_added')
         queryset = queryset.annotate(
             latest_sum=Subquery(subquery.values('sum')[:1]),
