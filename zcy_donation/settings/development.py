4d37fb908893a8aef9a5cd0371a389d18a7c2f3a
Sergey-sg <glushaksergey90@gmail.com>
2024-01-02 23:33:07 +0200

    Fixed env

--- zcy_donation/settings/development.py
+++ zcy_donation/settings/development.py
@@ -24,12 +24,12 @@ SECRET_KEY = getenv('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': getenv('MYSQL_DATABASE'),
-        'USER': getenv('MYSQL_USER'),
-        'PASSWORD': getenv('MYSQL_PASSWORD'),
-        'HOST': getenv('DB_HOST', 'mysqldb'),
+        'NAME': getenv('DB_NAME'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'HOST': getenv('DB_HOST'),
         'PORT': getenv('DB_PORT', '3306'),
     }
 }
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'db']
+ALLOWED_HOSTS = ['0.0.0.0']
