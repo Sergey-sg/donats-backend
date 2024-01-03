adc1a5415164cff2c574c3f5e2669c9ad02a75f9
Sergey-sg <glushaksergey90@gmail.com>
2024-01-03 20:54:00 +0200

    added JarAlbum inlines in admin to Jar

--- apps/jars/admin.py
+++ apps/jars/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from adminsortable2.admin import SortableAdminMixin
 
-from .models import Jar, JarTag
+from .models import Jar, JarTag, JarAlbum
+
+
+class JarAlbumAdmin(admin.StackedInline):
+    model = JarAlbum
+    extra = 1
 
 
 @admin.register(Jar)
 class JarAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ['title', 'monobank_id', 'goal', 'date_added', 'dd_order']
     search_fields = ['monobank_id', 'title', 'tags']
+    inlines = [JarAlbumAdmin]
     exclude = ['dd_order']
     ordering = ['dd_order', '-date_added']
 
